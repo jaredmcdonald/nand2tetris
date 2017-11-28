e40b182f8def00e85fcaccc75dfc0d25a5d01c4e
Jared McDonald <jared.r.mcdonald@gmail.com>
2017-11-28 12:07:17 -0500

    use ? operator and unwrap_or

--- vm-translator-rs/src/main.rs
+++ vm-translator-rs/src/main.rs
@@ -11,53 +11,47 @@ use glob::glob;
 use std::path::Path;
 use std::env::args;
 use std::fs::{File, metadata};
-use std::io::{BufReader, BufRead};
+use std::io::{BufReader, BufRead, Result};
 
-fn compile_file(filename: &str) -> Vec<String> {
-    match File::open(&filename) {
-        Ok(f) => {
-            let file = BufReader::new(&f);
-            let lines: Vec<String> = file.lines().map(|line| line.unwrap()).collect();
-            // haha, ugh - what's the idiomatic way to avoid all this 👇 ?
-            let name = Path::new(&filename).file_stem().unwrap().to_str().unwrap();
-            generate(
-                parse(lines.as_slice()).as_slice(),
-                name
-            )
-        },
-        Err(e) => {
-            panic!("Error opening file {}: {:?}", filename, e);
-        },
-    }
+fn compile_file(filename: &str) -> Result<Vec<String>> {
+    let f = File::open(&filename)?;
+    let file = BufReader::new(&f);
+    // todo: see if i can eliminate this unwrap            👇
+    let lines: Vec<String> = file.lines().map(|line| line.unwrap()).collect();
+    let name = Path::new(&filename).file_stem().unwrap().to_str().unwrap();
+    Ok(generate(
+        parse(lines.as_slice()).as_slice(),
+        name
+    ))
 }
 
 fn compile_directory(directory_path: &str) -> Vec<String> {
     glob(&format!("{}/*.vm", directory_path)).unwrap()
-        .flat_map(|path| compile_file(path.unwrap().to_str().unwrap())) // what could possibly go wrong? 🙀
+        .flat_map(|path| compile_file(path.unwrap().to_str().unwrap()).unwrap()) // what could possibly go wrong? 🙀
         .collect() // is there a way to return this without collecting?
 }
 
-fn main() {
+fn compile() -> Result<()> {
     // is there a more idiomatic way to concatenate `Vec`s than `let mut` + `extend`?
     let mut lines = bootstrap();
-    lines.extend(
-        // how to avoid all this `if let` nesting when we have a bunch of dependent results/options/etc?
-        if let Some(target) = args().nth(1) {
-            if let Ok(fs_metadata) = metadata(&target) {
-                if fs_metadata.is_dir() {
-                    compile_directory(&target)
-                } else {
-                    compile_file(&target)
-                }
-            } else {
-                panic!("error reading path {}", target);
-            }
-        } else {
-            panic!("must specify a filename or directory target as the first argument");
-        }
-    );
+    let target = args().nth(1).unwrap_or(".".to_string());
+    let fs_metadata = metadata(&target)?;
+    let additional_lines = if fs_metadata.is_dir() {
+        compile_directory(&target)
+    } else {
+        compile_file(&target)?
+    };
+    lines.extend(additional_lines);
     for line in lines {
         // just print assembly to stdout, let the user direct it where she prefers
         println!("{}", line)
+    }
+    Ok(())
+}
+
+fn main() {
+    match compile() {
+        Ok(_) => (),
+        Err(e) => panic!("{}", e),
     }
 }
